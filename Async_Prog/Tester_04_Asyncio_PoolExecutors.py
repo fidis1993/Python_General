2a1626813ec2d7a15dcdb8814c8f0980c9f97356
Themis Sarafidis <sarafidis12@gmail.com>
2024-06-03 12:27:00 +0300

    Perfected Tester4
    -It works
    - ThreadedPoolExecution
    - Asyncio

--- Async_Prog/Tester_04_Asyncio_PoolExecutors.py
+++ Async_Prog/Tester_04_Asyncio_PoolExecutors.py
@@ -1,18 +1,26 @@
+# Example with ThreadPoolExecutor
+
 from concurrent.futures import ThreadPoolExecutor
 from functions_classes import MultiTimer
 
 some_values = [3,4,5,6,7,8,9]
 
 def cube(num):
-    print (f" Cube of {num}^3 = {num^3}")
+    print (f" Cube of {num}^3 = {num**3}")
+    return num**3
 
 def main():
     results = []
     with ThreadPoolExecutor(max_workers = 5) as exe:
         results.append(exe.submit(cube,2))
-        
-    # Map the method cube with a list of values 
-    results.append(exe.map(cube, some_values))
+            
+        # Map the method cube with a list of values 
+        results.append(exe.map(cube, some_values))
 
+    print ("Printing cube of 2-9: ")
     for result in results:
         print (result)
+if __name__ == "__main__":
+    timer =  MultiTimer()
+    main()
+    timer.time_calculate()
